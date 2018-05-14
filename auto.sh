93639b24b69e672d942b2b9c461d13db3287c7f8
Declan Pym <declanpym@gmail.com>
2018-05-14 21:51:20 +1000

    Auto script
    shitty bash script for installs, requires /tmp/logstash-forwarder.crt be copied over before hand - this can be found in the password manager.
    
    Signed-off-by: Declan Pym <declanpym@gmail.com>

---
+++ auto.sh
@@ -0,0 +1,28 @@
+curl -O https://raw.githubusercontent.com/Impulss/beats/master/filebeat.yml
+curl -O https://raw.githubusercontent.com/Impulss/beats/master/packetbeat.yml 
+curl -O https://raw.githubusercontent.com/Impulss/beats/master/topbeat.yml
+
+sudo mkdir -p /etc/pki/tls/certs
+
+sudo cp /tmp/logstash-forwarder.crt /etc/pki/tls/certs/
+
+sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
+
+cat >/etc/yum.repos.d/elastic-beats.repo <<EOL
+[beats]
+name=Elastic Beats Repository
+baseurl=https://packages.elastic.co/beats/yum/el/\$basearch
+enabled=1
+gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch
+gpgcheck=1
+EOL 
+
+sudo yum -y install filebeat packetbeat topbeat
+
+mv filebeat.yml /etc/filebeat/filebeat.yml
+mv packetbeat.yml /etc/packetbeat/packetbeat.yml
+mv topbeat.yml /etc/topbeat/topbeat.yml
+
+service filebeat start
+service packetbeat start
+service topbeat start
